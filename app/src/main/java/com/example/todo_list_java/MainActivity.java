7909919caa5a77673ec70956dd33304b1434e7a5
gwise <pulmuone.ji@gmail.com>
2020-08-09 23:13:22 +0900

    1

--- app/src/main/java/com/example/todo_list_java/MainActivity.java
+++ app/src/main/java/com/example/todo_list_java/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.todo_list_java;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.room.Room;
 
@@ -9,27 +10,33 @@ import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.todo_list_java.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
-    private EditText mTodoEditText;
-    private TextView mResultTextView;
+//    private EditText mTodoEditText;
+//    private TextView mResultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setLifecycleOwner(this);
 
         MainViewModel viewModel = new ViewModelProvider(this, new ViewModelProvider.AndroidViewModelFactory(getApplication())).get(MainViewModel.class);
 
-        mTodoEditText = findViewById(R.id.todo_edit);
-        mResultTextView = findViewById(R.id.result_text);
+        binding.setViewModel(viewModel);
 
-        viewModel.getAll().observe(this, todos -> {
-           mResultTextView.setText(todos.toString());
-        });
-
-        findViewById(R.id.add_button).setOnClickListener(v -> {
-            viewModel.insert(new Todo(mTodoEditText.getText().toString()));
-        });
+//        mTodoEditText = findViewById(R.id.todo_edit);
+//        mResultTextView = findViewById(R.id.result_text);
+//
+//        viewModel.getAll().observe(this, todos -> {
+//            binding.resultText.setText(todos.toString());
+//        });
+//
+//        findViewById(R.id.add_button).setOnClickListener(v -> {
+//            viewModel.insert(new Todo(binding.todoEdit.getText().toString()));
+//        });
     }
 }
